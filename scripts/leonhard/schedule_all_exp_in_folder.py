bc451dd9f62180fad866ad2c55189aaecb453b2b
JonasFrey96 <frey.breitenbrunn@gmx.de>
2020-11-19 23:32:31 +0000

    small changes for leonhard submit file

--- scripts/leonhard/schedule_all_exp_in_folder.py
+++ scripts/leonhard/schedule_all_exp_in_folder.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 import argparse
+import yaml
+from os.path import expanduser
 """
 For execution run:
 python scripts/leonhard/schedule_all_exp_in_folder.py --exp=t24h --time=24
@@ -13,8 +15,10 @@ parser.add_argument('--time', default=24, required=True,
                     help='Runtime.')
 parser.add_argument('--mem', default=10240, help='Min GPU Memory')
 parser.add_argument('--model_base_path', default=None, help='Path tp models/runs folder')
+parser.add_argument('--out', default=True)
 
 args = parser.parse_args()
+out = args.out
 mp = args.model_base_path 
 print(args.time)
 mem = args.mem
@@ -29,13 +33,15 @@ elif  isinstance( args.time, str):
   s1 = args.time
 else:
   raise Exception
-from os.path import expanduser
+
 home = expanduser("~")
 
 p = f'{home}/PLR3/yaml/exp/{args.exp}/'
 exps = [str(p) for p in Path(p).rglob('*.yml')]
 
-import yaml
+
+model_paths = []
+
 for j,e in enumerate(exps):
   print(e)
   with open(e) as f:
@@ -44,11 +50,22 @@ for j,e in enumerate(exps):
   if not (mp is None):
     p =  doc['model_path'].find('/models/runs') #+ int( len('models/runs') )
     doc['model_path'] =  mp + doc['model_path'][p:] 
+
   with open(e, 'w+') as f:
+    model_paths.append( doc['model_path'] )
     yaml.dump(doc, f, default_flow_style=False, sort_keys=False)
   
 
 for j, e in enumerate(exps):
-  cmd = f"""cd {home}/PLR3 && bsub -n 20 -W {s1} -R "rusage[mem=3000,ngpus_excl_p=1]" -R "select[gpu_mtotal0>={mem}]" -R "rusage[scratch=16000]" ./scripts/leonhard/submit.sh --exp={e} --env=yaml/env/env_leonhard_jonas.yml"""
+  if out:
+    p = model_paths[j].split('/')
+    p = '/'.join(p[:-1])
+    Path(p).mkdir(parents=True, exist_ok=True)
+
+    name = model_paths[j].split('/')[-1] + str(j) + '.out'
+    o = f""" -oo {p}/{name} """
+  else:
+    o = ' '
+  cmd = f"""cd {home}/PLR3 && bsub{o}-n 20 -W {s1} -R "rusage[mem=3000,ngpus_excl_p=1]" -R "select[gpu_mtotal0>={mem}]" -R "rusage[scratch=16000]" ./scripts/leonhard/submit.sh --exp={e} --env=yaml/env/env_leonhard_jonas.yml"""
   os.system(cmd)
   print(f'Run: {j}, Exp: {e}, Time: {s1}h')
